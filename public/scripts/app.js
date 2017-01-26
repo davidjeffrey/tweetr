d7c98900a7df7304abcdac780649b0bd8f676181
David Jeffrey <dsejeffrey@gmail.com>
2017-01-26 22:05:58 +0000

    last commit missed some stuff. everything in a basic finished state.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,15 @@
 $(function() {
 
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
+  function escape(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
 
-function timeSince(date) {
+  function timeSince(date) {
+    let seconds = Math.floor((new Date() - date) / 1000);
 
-    var seconds = Math.floor((new Date() - date) / 1000);
-
-    var interval = Math.floor(seconds / 31536000);
+    let interval = Math.floor(seconds / 31536000);
 
     if (interval > 1) {
         return interval + " years";
@@ -32,81 +31,71 @@ function timeSince(date) {
         return interval + " minutes";
     }
     return Math.floor(seconds) + " seconds";
-}
-
-console.log(timeSince($.now()))
-function createTweetElement (data) {
-  let result = $(`<article class="tweet">
-                    <header>
-                      <img src="${data.user.avatars.small}">
-                      <h3>${escape(data.user.name)}</h3>
-                      <div>${escape(data.user.handle)}</div>
-                    </header>
-                    <p>${escape(data.content.text)}</p>
-                    <footer>
-                        ${timeSince(escape(data.created_at))}
-                      <a href="#">
-                        <span class="glyphicon glyphicon-asterisk"></span>
-                      </a>
-                      <a href="#">
-                        <span class="glyphicon glyphicon glyphicon-refresh"></span>
-                      </a>
-                      <a href="#">
-                        <span class="glyphicon glyphicon-heart"></span>
-                      </a>
-                    </footer>
-                  </article>`
-                );
-  return result;
-}
-
-function renderTweets(tweets) {
-  $('#tweets-container').empty();
-  for (tweet of tweets) {
-    let $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
   }
-}
 
-$(".new-tweet input").on("click", function(){
-  event.preventDefault();
-  if ($(".new-tweet textarea").serialize().length - 5 > 140) {
-    alert("Too long! Must be less than 140 characters child!")
-  } else if (!$(".new-tweet textarea").val()) {
-    alert("Can't have an empty tweet child!")
-  } else {
-      $.post( "./tweets", $(".new-tweet textarea").serialize())
-      .success( function() {
-        $(".new-tweet").slideToggle(500)
-        $(".new-tweet textarea").val("")
-        loadTweets();
-        });
+  function createTweetElement (data) {
+    let result = $(`<article class="tweet">
+                      <header>
+                        <img src="${data.user.avatars.small}">
+                        <h3>${escape(data.user.name)}</h3>
+                        <div>${escape(data.user.handle)}</div>
+                      </header>
+                      <p>${escape(data.content.text)}</p>
+                      <footer>
+                          ${timeSince(escape(data.created_at))}
+                        <a href="#">
+                          <span class="glyphicon glyphicon-asterisk"></span>
+                        </a>
+                        <a href="#">
+                          <span class="glyphicon glyphicon glyphicon-refresh"></span>
+                        </a>
+                        <a href="#">
+                          <span class="glyphicon glyphicon-heart"></span>
+                        </a>
+                      </footer>
+                    </article>`
+                  );
+    return result;
+  }
+
+  function renderTweets(tweets) {
+    $('#tweets-container').empty();
+    for (tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
     }
-})
+  }
 
-
-
-function loadTweets () {
+  function loadTweets () {
   $.ajax({
       url: './tweets',
       method: 'GET',
       success: function (tweets) {
         renderTweets(tweets.reverse())}
       })
-}
+  }
 
-loadTweets();
+  $(".new-tweet input").on("click", function(){
+    event.preventDefault();
+    if ($(".new-tweet textarea").serialize().length - 5 > 140) {
+      alert("Too long! Must be less than 140 characters child!")
+    } else if (!$(".new-tweet textarea").val()) {
+      alert("Can't have an empty tweet child!")
+    } else {
+        $.post( "./tweets", $(".new-tweet textarea").serialize())
+        .success( function() {
+          $(".new-tweet").slideToggle(500)
+          $(".new-tweet textarea").val("")
+          loadTweets();
+          });
+      }
+  });
 
-$("#nav-bar button").on("click", function() {
-  $(".new-tweet").slideToggle(250)
-  $(".new-tweet textarea").focus()
-})
+  $("#nav-bar button").on("click", function() {
+    $(".new-tweet").slideToggle(250)
+    $(".new-tweet textarea").focus()
+  });
 
-})
+  loadTweets();
 
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+});
